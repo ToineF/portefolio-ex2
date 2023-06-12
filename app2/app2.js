517dc6e54013bf2c36f14bc9a53d2b24faa50316
ToineF <antoine.foucault777@gmail.com>
2023-06-12 10:44:33 +0200

    bmi calculator

---
+++ app2/app2.js
@@ -0,0 +1,17 @@
+const height = document.querySelector("#height")
+const weight = document.querySelector("#weight")
+const submitButton = document.querySelector("#submitButton")
+const answer = document.querySelector("#answer")
+
+console.log(height.value)
+submitButton.addEventListener('click', () => {
+    const heightValue = height.value / 100
+    const weightValue = weight.value
+    if (isNaN(heightValue) || isNaN(weightValue) 
+    || !heightValue || !weightValue) {
+        preventDefault()
+    }
+    let imc = weightValue / (Math.pow(heightValue,2))
+    imc = imc.toFixed(1)
+    answer.innerText = `Votre IMC est de ${imc}`
+})
